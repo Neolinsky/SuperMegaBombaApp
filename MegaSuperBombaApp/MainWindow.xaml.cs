7c194c87d0cc96ea39715ad681ded134277b76c5
Andrey <andreywatchforyou@gmail.com>
2021-09-02 16:52:38 +0500

    Just a template

---
+++ MegaSuperBombaApp/MainWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MegaSuperBombaApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // need to maximize and getting winodw back to normal 
+        bool maximized = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDevision_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDote_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEqual_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnfullScreen_Click(object sender, RoutedEventArgs e)
+        {
+         
+            if (maximized != true)
+            {
+                this.WindowState = WindowState.Maximized;
+                
+                
+                maximized = !maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+                maximized = !maximized;
+
+
+            }
+
+
+
+
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+                if (e.ChangedButton == MouseButton.Left)
+                    this.DragMove();
+            
+        }
+    }
+}
