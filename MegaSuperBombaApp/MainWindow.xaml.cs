b7f2dfdbed62a12cfa8d2316cec445cdb4f2833c
Andrey <andreywatchforyou@gmail.com>
2021-09-03 15:50:38 +0500

    Woked a litle bit more on the MegaSuperBombaApp

--- MegaSuperBombaApp/MainWindow.xaml.cs
+++ MegaSuperBombaApp/MainWindow.xaml.cs
@@ -23,6 +23,21 @@ namespace MegaSuperBombaApp
         // need to maximize and getting winodw back to normal 
         bool maximized = false;
 
+        string value;
+
+        // Used to clear gritting from textbox
+        bool beenPressedAnyButton = false;
+
+
+        double result;
+        double currentNumber;
+
+        bool beenAssigned = false;
+
+        bool beenDoneOperations = false;
+
+        char lastOperation;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -32,82 +47,372 @@ namespace MegaSuperBombaApp
 
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
+            if(beenPressedAnyButton == false)
+            {
+                value = "";
 
+                value = value + "1";
+                TextNumbers.Text = value;
+                beenPressedAnyButton = true;
+                beenAssigned = true;
+            }
+
+            else
+            {
+                value = value + "1"; 
+                TextNumbers.Text = value;
+                beenAssigned = true;
+
+            }
         }
 
         private void btn2_Click(object sender, RoutedEventArgs e)
         {
+            if (beenPressedAnyButton == false)
+            {
+                value = "";
 
+                value = value + "2";
+                TextNumbers.Text = value;
+                beenPressedAnyButton = true;
+                beenAssigned = true;
+
+            }
+
+            else
+            {
+                value = value + "2";
+                TextNumbers.Text = value;
+                beenAssigned = true;
+
+            }
         }
 
         private void btn3_Click(object sender, RoutedEventArgs e)
         {
+            if (beenPressedAnyButton == false)
+            {
+                value = "";
 
+                value = value + "3";
+                TextNumbers.Text = value;
+                beenPressedAnyButton = true;
+                beenAssigned = true;
+
+            }
+
+            else
+            {
+                value = value + "3";
+                TextNumbers.Text = value;
+                beenAssigned = true;
+
+            }
         }
 
         private void btnDevision_Click(object sender, RoutedEventArgs e)
         {
+            if (beenAssigned == false)
+            {
+                value = "First input value you are adding to";
+                TextNumbers.Text = value;
 
+            }
+            else
+            {
+                if (beenDoneOperations == false)
+                {
+                    result = Double.Parse(value);
+                    beenDoneOperations = true;
+                    value = "";
+                    lastOperation = '/';
+                }
+                else
+                {
+
+                    lastOperation = '/';
+                    value = "";
+                }
+            }
         }
 
         private void btn4_Click(object sender, RoutedEventArgs e)
         {
+            if (beenPressedAnyButton == false)
+            {
+                value = "";
 
+                value = value + "4";
+                TextNumbers.Text = value;
+                beenPressedAnyButton = true;
+                beenAssigned = true;
+
+            }
+
+            else
+            {
+                value = value + "4";
+                TextNumbers.Text = value;
+                beenAssigned = true;
+            }
         }
 
         private void btn5_Click(object sender, RoutedEventArgs e)
         {
+            if (beenPressedAnyButton == false)
+            {
+                value = "";
 
+                value = value + "5";
+                TextNumbers.Text = value;
+                beenPressedAnyButton = true;
+                beenAssigned = true;
+
+            }
+
+            else
+            {
+                value = value + "5";
+                TextNumbers.Text = value;
+                beenAssigned = true;
+            }
         }
 
         private void btn6_Click(object sender, RoutedEventArgs e)
         {
+            if (beenPressedAnyButton == false)
+            {
+                value = "";
 
+                value = value + "6";
+                TextNumbers.Text = value;
+                beenAssigned = true;
+                beenPressedAnyButton = true;
+            }
+
+            else
+            {
+                value = value + "6";
+                TextNumbers.Text = value;
+                beenAssigned = true;
+            }
         }
 
         private void btnMultiply_Click(object sender, RoutedEventArgs e)
         {
+            if (beenAssigned == false)
+            {
+                value = "First input value you are adding to";
+                TextNumbers.Text = value;
 
+            }
+            else
+            {
+                if (beenDoneOperations == false)
+                {
+                    result = Double.Parse(value);
+                    beenDoneOperations = true;
+                    value = "";
+                    lastOperation = '*';
+                }
+                else
+                {
+
+                    lastOperation = '*';
+                    value = "";
+                }
+            }
         }
 
         private void btn7_Click(object sender, RoutedEventArgs e)
         {
+            if (beenPressedAnyButton == false)
+            {
+                value = "";
 
+                value = value + "7";
+                TextNumbers.Text = value;
+                beenPressedAnyButton = true;
+                beenAssigned = true;
+            }
+
+            else
+            {
+                value = value + "7";
+                TextNumbers.Text = value;
+                beenAssigned = true;
+            }
         }
 
         private void btn8_Click(object sender, RoutedEventArgs e)
         {
+            if (beenPressedAnyButton == false)
+            {
+                value = "";
 
+                value = value + "8";
+                TextNumbers.Text = value;
+                beenPressedAnyButton = true;
+                beenAssigned = true;
+            }
+
+            else
+            {
+                value = value + "8";
+                TextNumbers.Text = value;
+                beenAssigned = true;
+            }
         }
 
         private void btn9_Click(object sender, RoutedEventArgs e)
         {
+            if (beenPressedAnyButton == false)
+            {
+                value = "";
 
+                value = value + "9";
+                TextNumbers.Text = value;
+                beenPressedAnyButton = true;
+                beenAssigned = true;
+            }
+
+            else
+            {
+                value = value + "9";
+                TextNumbers.Text = value;
+                beenAssigned = true;
+            }
         }
 
         private void btnPlus_Click(object sender, RoutedEventArgs e)
         {
-
+            if(beenAssigned == false)
+            {
+                value = "First input value you are adding to";
+                TextNumbers.Text = value;
+                
+            }
+            else
+            {
+                if(beenDoneOperations == false)
+                {
+                    result = Double.Parse(value);
+                    beenDoneOperations = true;
+                    value = "";
+                    lastOperation = '+';
+                }
+                else
+                {
+                   
+                    lastOperation = '+';
+                    value = "";
+                }
+            }
         }
 
         private void btnDote_Click(object sender, RoutedEventArgs e)
         {
+            if (beenPressedAnyButton != false)
+            {
+              
+                    value = value + ".";
+                    TextNumbers.Text = value;
+                
+               
+            }
+            else{
 
+                TextNumbers.Text = "Can't start frome dote";
+            }
+
+            
         }
 
         private void btn0_Click(object sender, RoutedEventArgs e)
         {
+            if (beenPressedAnyButton == false)
+            {
+                value = "";
 
+                value = value + "0";
+                TextNumbers.Text = value;
+                beenPressedAnyButton = true;
+            }
+
+            else
+            {
+                value = value + "0";
+                TextNumbers.Text = value;
+
+            }
         }
 
         private void btnEqual_Click(object sender, RoutedEventArgs e)
         {
+           switch (lastOperation)
+            {
+                case '+':
+                    currentNumber = Double.Parse(value);
+                    result = result + currentNumber;
+                    TextNumbers.Text = result.ToString();
+                    break;
 
+                case '-':
+                    currentNumber = Double.Parse(value);
+                    result = result - currentNumber;
+                    TextNumbers.Text = result.ToString();
+                    break;
+
+                case '*':
+                    currentNumber = Double.Parse(value);
+                    result = result * currentNumber;
+                    TextNumbers.Text = result.ToString();
+                    break;
+
+                case '/':
+                    currentNumber = Double.Parse(value);
+                    if(currentNumber != 0)
+                    {
+                        result = result / currentNumber;
+                        TextNumbers.Text = result.ToString();
+                        break;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Can't devide by 0");
+                        value = "";
+                        TextNumbers.Text = "Input another value!";
+                        beenPressedAnyButton = false;
+                    }
+                    break;
+
+            }
         }
 
         private void btnMinus_Click(object sender, RoutedEventArgs e)
         {
+            if (beenAssigned == false)
+            {
+                value = "First input value you are adding to";
+                TextNumbers.Text = value;
 
+            }
+            else
+            {
+                if (beenDoneOperations == false)
+                {
+                    result = Double.Parse(value);
+                    beenDoneOperations = true;
+                    value = "";
+                    lastOperation = '-';
+                }
+                else
+                {
+
+                    lastOperation = '-';
+                    value = "";
+                }
+            }
         }
 
         private void btnfullScreen_Click(object sender, RoutedEventArgs e)
